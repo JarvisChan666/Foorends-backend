0048c82368421ce169c4deeb4f9d84208cadc7f0
JarvisChan666 <744765386@qq.com>
2024-07-04 20:47:20 +0800

    initialize mongodb

---
+++ README.md
@@ -0,0 +1,9 @@
+## Tech Stack in Backend
+- Server: Node.js, Express,TypeScript
+- Database: MongoDB
+- Auth
+
+
+## Quick Start
+1. yarn
+2. yarn dev
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
-import express, {Request, Response} from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("Connected to database!"));
 
 const app = express();
-app.use(express.json());// Turn request to json
+app.use(express.json()); // Turn request to json
 app.use(cors());
 
 //Backend gets request from "/test" and response
-app.get("/test", async(req:Request, res: Response) => {
-    res.json({message:"hi"})
-})
+app.get("/test", async (req: Request, res: Response) => {
+  res.json({ message: "hi" });
+});
+
+app.listen(7000, () => {
+  console.log("server started on localhost:7000");
+});
